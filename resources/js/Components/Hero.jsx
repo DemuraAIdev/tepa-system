daa5c4cec78f4856e121a776178fb9816b48fa36
Haekal Abdillah Ramadhan <ekal26b@gmail.com>
2024-04-23 09:35:56 +0700

    Translation-success-1

--- resources/js/Components/Hero.jsx
+++ resources/js/Components/Hero.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import { useEffect } from 'react';
-import Typed from 'typed.js';
+import React from "react";
+import { useEffect } from "react";
+import Typed from "typed.js";
 import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa6";
 import { motion } from "framer-motion";
-import { Link as ScrollLink } from 'react-scroll';
+import { Link as ScrollLink } from "react-scroll";
+import { useTranslation } from "react-i18next";
 
 const items = {
     hidden: {
@@ -14,11 +15,10 @@ const items = {
         opacity: 1,
         y: 0,
         transition: {
-            duration: .3,
-        }
-    }
-
-}
+            duration: 0.3,
+        },
+    },
+};
 
 const items2 = {
     hidden: {
@@ -29,11 +29,10 @@ const items2 = {
         opacity: 1,
         y: 0,
         transition: {
-            duration: .4,
-        }
-    }
-
-}
+            duration: 0.4,
+        },
+    },
+};
 
 const items3 = {
     hidden: {
@@ -44,34 +43,36 @@ const items3 = {
         opacity: 1,
         y: 0,
         transition: {
-            duration: .5,
-        }
-    }
-
-}
+            duration: 0.5,
+        },
+    },
+};
 
 const Hero = () => {
-
     useEffect(() => {
         VanillaTilt.init(document.querySelectorAll(".card"), {
             max: 5,
             speed: 10,
             glare: true,
-            "max-glare": 0.10
-        })
-    })
+            "max-glare": 0.1,
+        });
+    });
 
     // Create reference to store the DOM element containing the animation
     const el = React.useRef(null);
+    const { t, i18n } = useTranslation(["global"]);
 
     React.useEffect(() => {
         const typed = new Typed(el.current, {
-            strings: ["Welcome to Raflesia Technopark's Official Website!", "Your All-in-One School Resource Center", "Unlocking School Convenience"],
+            strings: [
+                "Welcome to Raflesia Technopark's Official Website!",
+                "Your All-in-One School Resource Center",
+                "Unlocking School Convenience",
+            ],
             typeSpeed: 50,
             smartBackspace: true,
             backDelay: 3500,
             loop: Infinity,
-
         });
 
         return () => {
@@ -81,25 +82,35 @@ const Hero = () => {
     }, []);
 
     return (
-        <div className='flex flex-col gap-5 md:w-[50%] p-7 lg:ml-[5em] order-2 md:order-1  rounded-lg'>
-            <h1 className='text-4xl font-body font-bold text-indigo-500'>
+        <div className="flex flex-col gap-5 md:w-[50%] p-7 lg:ml-[5em] order-2 md:order-1  rounded-lg">
+            <h1 className="text-4xl font-body font-bold text-indigo-500">
                 <span ref={el} />
             </h1>
-            <p className='text-justify '>
-                Technopark, our school's multifunctional resource center, is dedicated to providing a seamless and enhanced school experience. From high-quality uniforms and comprehensive office supplies to electronics and efficient fee payment through the mini bank, along with photocopy services and expert educational travel planning, Tec   hnopark is your one-stop destination for academic and administrative support, aiming to elevate every aspect of your school journey.
-
-            </p>
+            <p className="text-justify ">{t("welcome")}</p>
             <div>
-                <ScrollLink to="cards" spy={true} smooth={true} href='#cards' offset={-150} duration={500}>
-                    <button className='btn border-indigo-500 text-white hover:text-white font-bold bg-indigo-600 hover:scale-[1.05] '>
+                <ScrollLink
+                    to="cards"
+                    spy={true}
+                    smooth={true}
+                    href="#cards"
+                    offset={-150}
+                    duration={500}
+                >
+                    <button className="btn border-indigo-500 text-white hover:text-white font-bold bg-indigo-600 hover:scale-[1.05] ">
                         Read More!
                     </button>
                 </ScrollLink>
             </div>
-            <div className='w-fit flex gap-4'>
-                <a href='https://www.facebook.com/Smkn1KotaBengkulu/?locale=id_ID' aria-label='fesfsefesf' className='whitespace-no-wrap overflow-hidden' target='_blank' rel='noopener noreferrer'>
+            <div className="w-fit flex gap-4">
+                <a
+                    href="https://www.facebook.com/Smkn1KotaBengkulu/?locale=id_ID"
+                    aria-label="fesfsefesf"
+                    className="whitespace-no-wrap overflow-hidden"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <motion.div
-                        className='text-sm text-white rounded-full bg-indigo-600 p-2'
+                        className="text-sm text-white rounded-full bg-indigo-600 p-2"
                         variants={items}
                         initial="hidden"
                         animate="show"
@@ -108,28 +119,40 @@ const Hero = () => {
                     </motion.div>
                 </a>
 
-                <a href='https://www.instagram.com/smkn1kotabengkulu/' aria-label='fesfsefesf' target='_blank' rel='noopener noreferrer'>
+                <a
+                    href="https://www.instagram.com/smkn1kotabengkulu/"
+                    aria-label="fesfsefesf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <motion.div
-                        className='text-sm text-white rounded-full bg-pink-600 p-2'
+                        className="text-sm text-white rounded-full bg-pink-600 p-2"
                         variants={items2}
                         initial="hidden"
-                        animate="show">
+                        animate="show"
+                    >
                         <FaInstagram />
                     </motion.div>
                 </a>
 
-                <a href='https://www.youtube.com/channel/UCydN3u2tCciDrJKo06ug-oQ' aria-label='fesfsefesf' target='_blank' rel='noopener noreferrer'>
+                <a
+                    href="https://www.youtube.com/channel/UCydN3u2tCciDrJKo06ug-oQ"
+                    aria-label="fesfsefesf"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
                     <motion.div
-                        className='text-sm text-white rounded-full bg-red-600 p-2 hover:scale-50'
+                        className="text-sm text-white rounded-full bg-red-600 p-2 hover:scale-50"
                         variants={items3}
                         initial="hidden"
-                        animate="show">
+                        animate="show"
+                    >
                         <FaYoutube />
                     </motion.div>
                 </a>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Hero
+export default Hero;
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -7,26 +7,8 @@ import { createInertiaApp } from "@inertiajs/react";
 import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
 import AOS from "aos";
 import "aos/dist/aos.css";
-
-import global_id from "../translations/id/global.json";
-import global_en from "../translations/en/global.json";
-import i18next from "i18next";
 import { interpolate } from "framer-motion";
-
-// Translation using i18next-React -----
-
-i18next.init({
-    interpolation: { escapeValue: false },
-    lng: "en",
-    rescources: {
-        en: {
-            global: global_en,
-        },
-        es: {
-            global: global_id,
-        },
-    },
-});
+import "./i18n";
 
 const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
