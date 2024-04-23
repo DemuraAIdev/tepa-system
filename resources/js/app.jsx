964a6c867b944738c67eea3214b4b385c1e297ee
Haekal Abdillah Ramadhan <ekal26b@gmail.com>
2024-04-23 08:36:58 +0700

    translation-1

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -1,28 +1,49 @@
-import './bootstrap';
-import './credits';
-import '../css/app.css';
+import "./bootstrap";
+import "./credits";
+import "../css/app.css";
 
-import { createRoot } from 'react-dom/client';
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import AOS from 'aos';
-import 'aos/dist/aos.css';
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import { createRoot } from "react-dom/client";
+import { createInertiaApp } from "@inertiajs/react";
+import { resolvePageComponent } from "laravel-vite-plugin/inertia-helpers";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
+import global_id from "../translations/id/global.json";
+import global_en from "../translations/en/global.json";
+import i18next from "i18next";
+import { interpolate } from "framer-motion";
+
+// Translation using i18next-React -----
+
+i18next.init({
+    interpolation: { escapeValue: false },
+    lng: "en",
+    rescources: {
+        en: {
+            global: global_en,
+        },
+        es: {
+            global: global_id,
+        },
+    },
+});
+
+const appName = import.meta.env.VITE_APP_NAME || "Laravel";
 
 AOS.init();
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.jsx`, import.meta.glob('./Pages/**/*.jsx')),
+    resolve: (name) =>
+        resolvePageComponent(
+            `./Pages/${name}.jsx`,
+            import.meta.glob("./Pages/**/*.jsx")
+        ),
     setup({ el, App, props }) {
         const root = createRoot(el);
 
-        root.render(
-            
-            <App  {...props} />
-        );
+        root.render(<App {...props} />);
     },
     progress: {
-        color: '#4f46e5',
+        color: "#4f46e5",
     },
 });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
     ],
     theme: {
         fontFamily: {
-            'body': ["Poppins", 'sans-serif'],
-            'heading': ["Montserrat", 'sans-serif'],
+            body: ["Poppins", "sans-serif"],
+            heading: ["Montserrat", "sans-serif"],
         },
         extend: {
-            'hero-pattern': "url('/Image/Hero.jpg')",
+            "hero-pattern": "url('/Image/Hero.jpg')",
             gridAutoRows: {
-                'gridBox': 'minmax(160px, auto)',
-            }
+                gridBox: "minmax(160px, auto)",
+            },
         },
         boxShadow: {
-            'cstm': '0px 3px 8px rgba(0,0,0,0.24)',
-            'cstm2': '0px 3px 8px rgba(0,0,0,0.15)'
-        }
+            cstm: "0px 3px 8px rgba(0,0,0,0.24)",
+            cstm2: "0px 3px 8px rgba(0,0,0,0.15)",
+        },
     },
     daisyui: {
         themes: ["dark"],
-      },
+    },
     // eslint-disable-next-line no-undef
-    plugins: [require('daisyui')],
-
-}
+    plugins: [require("daisyui")],
+};
